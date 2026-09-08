c47ce26ed76553db8d12057c0f887894f13acac3
sirlewis916-star <sirlewis916@gmail.com>
2026-09-08 18:17:40 +0300

    Add files via upload
     Add week 1 assignment - intro program

---
+++ intro.py
@@ -0,0 +1,10 @@
+name = "Lewis"
+age = 21
+favourite_language = "Python"
+goal = "to master Git, GitHub and become a full-stack developer"
+
+# This program introduces me and displays my course goal using variables
+print(f"Hello, my name is {name}.")
+print(f"I am {age} years old.")
+print(f"My favourite programming language is {favourite_language}.")
+print(f"My goal is {goal}.")
